21a98e5b1cd34b5713e08c7a85e824b413910f64
engjiosmani <154840265+engjiosmani@users.noreply.github.com>
2025-05-17 23:01:59 +0200

    shtimi i funksionit calculate_partial_sum për llogaritje të pjesshme me pthread

--- detyra.c
+++ detyra.c
@@ -14,3 +14,21 @@ int thread_id;
 int start_index;
 int end_index;
 } ThreadData;
+
+void* calculate_partial_sum(void* arg) {
+    ThreadData* data = (ThreadData*) arg;
+    int start = data->start_index;
+    int end = data->end_index;
+    double sum = 0;
+
+    for (int i = start; i < end; i++) {
+        sum += vector[i];
+    }
+
+    partial_sums[data->thread_id] = sum;
+
+    printf("Thread %d: start = %d, end = %d, sum = %.2f\n",
+           data->thread_id, start, end, sum);
+
+    pthread_exit(NULL);
+}
